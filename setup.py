ad6fdb7e27d3c72fe0d235b3f841f4313d6ec505
Alex Guerra <alex@heyimalex.com>
2015-07-22 20:56:10 -0500

    Bump version, minor setup.py modifications

--- setup.py
+++ setup.py
@@ -2,25 +2,25 @@ from setuptools import setup
 import multiprocessing
 import glob
 
-setup(name='rratelimit',
-      version='0.0.3',
-      description='Rate limiting classes for redis and redis-py',
-      url='http://github.com/HeyImAlex/rratelimit',
-      author='Alex Guerra',
-      author_email='alex@heyimalex.com',
-      classifiers=[
+setup(
+    name='rratelimit',
+    version='0.0.4',
+    description='Rate limiting classes for redis and redis-py',
+    url='http://github.com/HeyImAlex/rratelimit',
+    author='Alex Guerra',
+    author_email='alex@heyimalex.com',
+    classifiers=[
         'Development Status :: 4 - Beta',
         'License :: OSI Approved :: MIT License',
         'Intended Audience :: Developers',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-      ],
-      license='MIT',
-      packages=['rratelimit'],
-      zip_safe=False,
-      test_suite = 'nose.collector',
-      tests_require = ['nose>=1.0', 'redis'],
-      #package_data = {'rratelimit': glob.glob("rratelimit/lua/*.lua")},
-      include_package_data=True
-      
-    )
+    ],
+    license='MIT',
+    packages=['rratelimit'],
+    test_suite = 'nose.collector',
+    extras_require={
+        'test': ['nose>=1.0', 'redis'],
+    },
+    include_package_data=True
+)
